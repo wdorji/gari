696b2dad6e911f3bad8addbe5ad5e17271380d26
wdorji <wangdrak.dorji@gmail.com>
2024-01-31 20:58:01 -0500

    Initial render bug fix

--- gari/src/app/about/page.tsx
+++ gari/src/app/about/page.tsx
@@ -1,41 +1,80 @@
 "use client";
 
-import generatePanel from "../components/about/designs";
-import { ParallaxProvider, Parallax } from "react-scroll-parallax";
+import { ParallaxProvider, ParallaxBanner } from "react-scroll-parallax";
 
 export default function About() {
+  let isMobile = false;
+  if (typeof window !== "undefined") {
+    isMobile = window.innerWidth <= 768;
+  }
+
+  interface PanelProps {
+    imageUrl: string;
+    content: string;
+  }
+
+  function generatePanel(panelProps: PanelProps) {
+    return (
+      <ParallaxBanner
+        layers={[{ image: panelProps.imageUrl, speed: -20 }]}
+        style={{ aspectRatio: "2/1", height: "500px" }}
+      >
+        <div
+          style={{
+            position: "absolute",
+            inset: 0,
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center",
+          }}
+        >
+          <div
+            style={{
+              backgroundColor: "white",
+              padding: "20px",
+              border: "2px solid black",
+              width: "70%",
+            }}
+          >
+            <h1
+              style={{
+                fontSize: isMobile ? "sm" : "2rem",
+                color: "black",
+                fontWeight: "thin",
+              }}
+            >
+              {panelProps.content}
+            </h1>
+          </div>
+        </div>
+      </ParallaxBanner>
+    );
+  }
+
   const panel1props = {
     imageUrl:
       "https://images.unsplash.com/photo-1566020587334-bd85998d544e?auto=format&fit=crop&q=80&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&w=2833",
     content:
-      "Gari generates a map of roads in Bhutan highlighting areas with sharp turns and steep slopes.",
-    boxWidth: "900",
-    boxHeight: "125",
+      "Gari generates a map of roads in Bhutan highlighting points with sharp turns and steep slopes.",
   };
   const panel2props = {
+    content: `There is an alarming number of vehicles going off road from steep slopes which could be attributed to drinking, speeding, reckless driving, condition of the vehicle, roads, weather and environment. Regardless of the causes, Gari highlights high-risk zones where guardrails and other barriers must be placed to reduce fatalities.`,
     imageUrl:
-      "https://coolbackgrounds.io/images/backgrounds/white/pure-white-background-85a2a7fd.jpg",
-    content: `The reason why I created Gari was due to the alarming number of incidents where a vehicle veered off the road and I felt that this issue had to be addressed more. Although most of this could be attributed to the drivers from drinking, speeding or reckless driving, there could also be other external factors such as the condition of the vehicle, roads, weather and environment. Regardless of the causes, regions with sharp turns and areas of steep slops are high-risk zones which should have guardrails put in places as studies show that guardrails and other barriers can reduce fatalities.`,
-    boxWidth: "1300",
-    boxHeight: "350",
+      "https://i.pinimg.com/originals/84/2a/d6/842ad68b315b0f586c30b465221da609.jpg",
   };
 
   const panel3props = {
     imageUrl:
       "https://images.unsplash.com/photo-1622308933972-d07f377c6161?auto=format&fit=crop&q=80&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&w=2938",
     content:
-      "Gari (गारी) is the hindi word for car which is a loanword used by Bhutanese in informal contexts to which I felt was a more casual way to name this interface.",
-    boxWidth: "1200",
-    boxHeight: "125",
+      "Gari (गारी) is the hindi word for car which is a loanword used by most Bhutanese in informal contexts to refer to a car.",
   };
 
   const panel4props = {
-    imageUrl:
-      "https://coolbackgrounds.io/images/backgrounds/white/pure-white-background-85a2a7fd.jpg",
     content:
-      "In terms of extension, ideally a system to record a detailed entry about the accident location would help as there is a lack of data on locations of past car accidents to understand patterns in the landscape, climate and geography of these locations to identify potential high-risk zones for drivers. These locations can be prioritized first during projects involving road renovation. Better road infrastructure also would enhance economic development, accessibility and connectivity.",
-    boxWidth: "1250",
-    boxHeight: "300",
+      "Ideally a system to record a detailed entry about the accident location would help as there is a lack of data on locations of past car accidents to understand patterns in the landscape, climate and geography of these locations to identify potential high-risk zones for drivers. These locations can be prioritized first during projects involving road renovation. Better road infrastructure also would enhance economic development, accessibility and connectivity.",
+    imageUrl:
+      "https://i.pinimg.com/originals/84/2a/d6/842ad68b315b0f586c30b465221da609.jpg",
   };
 
   return (
--- gari/src/app/components/map/reactMap.tsx
+++ gari/src/app/components/map/reactMap.tsx
@@ -2,39 +2,24 @@
 
 import * as React from "react";
 import mapboxgl from "mapbox-gl";
-import { FeatureCollection } from "geojson";
 import "mapbox-gl/dist/mapbox-gl.css";
 import curves from "../../../../public/bt_curves.json";
 import slopes from "../../../../public/bt_slopes.json";
-import OpenWeatherAPI from "openweather-api-node";
+import ls from "../../../../public/ls.png";
+import curve from "../../../../public/curve.png";
+import dotenv from "dotenv";
+import { Spinner } from "@chakra-ui/react";
 
-const OPEN_WEATHER_API_KEY = "d76c019b0abe496ddafd4972749b4177";
-const MAP_ACCESS_TOKEN =
-  "pk.eyJ1Ijoid2RvcmppIiwiYSI6ImNsbGxnMzVkYzE3cGEzZHBmbTYxMDIzM3MifQ.qsQvGnT8BnGbcCDkxakeyQ";
+dotenv.config();
+
+const MAP_ACCESS_TOKEN = process.env.NEXT_PUBLIC_MAP_ACCESS_TOKEN;
+
 const MAP_STYLE = "mapbox://styles/mapbox/outdoors-v12?optimize=true";
-
-interface sidebarStats {
-  temperature: number;
-  weatherDescp: string;
-  humidity: number;
-  rain: number;
-  snow: number;
-  cloudiness: number;
-  visibility: number;
-}
 
 function MapboxMap() {
   // this is where the map instance will be stored after initialization
   const [map, setMap] = React.useState<mapboxgl.Map>();
-  const [userStats, setStats] = React.useState<sidebarStats>({
-    temperature: 0,
-    weatherDescp: "not loaded",
-    humidity: 0,
-    rain: 0,
-    snow: 0,
-    cloudiness: 0,
-    visibility: 0,
-  });
+  const [loading, setLoading] = React.useState(true);
 
   const [curLat, setLat] = React.useState(27.484506705905304);
   const [curLong, setLong] = React.useState(90.17388556333793);
@@ -63,95 +48,6 @@ function MapboxMap() {
       dragRotate: false,
       touchZoomRotate: false,
     });
-
-    // const size = 200;
-
-    // This implements `StyleImageInterface`
-    // // to draw a pulsing dot icon on the map.
-    // const pulsingDot = {
-    //   width: size,
-    //   height: size,
-    //   data: new Uint8Array(size * size * 4),
-
-    //   // When the layer is added to the map,
-    //   // get the rendering context for the map canvas.
-    //   onAdd: function () {
-    //     const canvas = document.createElement("canvas");
-    //     canvas.width = this.width;
-    //     canvas.height = this.height;
-    //     this.context = canvas.getContext("2d");
-    //   },
-
-    //   // Call once before every frame where the icon will be used.
-    //   render: function () {
-    //     const duration = 1000;
-    //     const t = (performance.now() % duration) / duration;
-
-    //     const radius = (size / 2) * 0.3;
-    //     const outerRadius = (size / 2) * 0.7 * t + radius;
-    //     const context = this.context;
-
-    //     // Draw the outer circle.
-    //     context.clearRect(0, 0, this.width, this.height);
-    //     context.beginPath();
-    //     context.arc(
-    //       this.width / 2,
-    //       this.height / 2,
-    //       outerRadius,
-    //       0,
-    //       Math.PI * 2
-    //     );
-    //     context.fillStyle = `rgba(0, 255, 255, ${1 - t})`;
-    //     context.fill();
-
-    //     // Draw the inner circle.
-    //     context.beginPath();
-    //     context.arc(this.width / 2, this.height / 2, radius, 0, Math.PI * 2);
-    //     context.fillStyle = "rgba(0, 255, 255, 1)";
-    //     context.strokeStyle = "white";
-    //     context.lineWidth = 2 + 4 * (1 - t);
-    //     context.fill();
-    //     context.stroke();
-
-    //     // Update this image's data with data from the canvas.
-    //     this.data = context.getImageData(0, 0, this.width, this.height).data;
-
-    //     // Continuously repaint the map, resulting
-    //     // in the smooth animation of the dot.
-    //     mapboxMap.triggerRepaint();
-
-    //     // Return `true` to let the map know that the image was updated.
-    //     return true;
-    //   },
-    // };
-
-    async function updateUserStats() {
-      // navigator.geolocation.getCurrentPosition(function (position) {
-      //   setLat(position.coords.latitude);
-      //   setLong(position.coords.longitude);
-      // });
-      let weather = new OpenWeatherAPI({
-        key: OPEN_WEATHER_API_KEY,
-        coordinates: {
-          lat: curLat,
-          lon: curLong,
-        },
-        units: "metric",
-      });
-      weather.getCurrent().then((data) => {
-        setStats({
-          temperature: data.weather.temp.cur,
-          weatherDescp: data.weather.description,
-          visibility: data.weather.visibility,
-          rain: data.weather.rain,
-          snow: data.weather.snow,
-          humidity: data.weather.humidity,
-          cloudiness: data.weather.clouds,
-        });
-      });
-    }
-    updateUserStats();
-    // setInterval(updateUserStats, 10000);
 
     mapboxMap.on("style.load", () => {
       if (typeof window === "undefined" || node === null) return;
@@ -182,6 +78,11 @@ function MapboxMap() {
       });
       mapboxMap.setTerrain({ source: "mapbox-dem", exaggeration: 1.5 });
     });
+
+    // map.loadImage(myImage, (error, image) => {
+    //   if (error) throw error;
+    //   map.addImage('image-name', image);
+    //   });
 
     mapboxMap.loadImage(
       "https://i.imgur.com/B9Z9vv9.png",
@@ -274,70 +175,19 @@ function MapboxMap() {
     return () => {
       mapboxMap.remove();
     };
-  }, [curLat, curLong]);
+  }, []);
 
-  function openNav() {
-    const mySidebarElement = document.getElementById("mySidebar");
-    if (mySidebarElement) {
-      mySidebarElement.style.width = "250px";
+  React.useEffect(() => {
+    const alreadyLoaded = localStorage.getItem("alreadyLoaded");
+    if (alreadyLoaded !== "true") {
+      localStorage.setItem("alreadyLoaded", "true");
+      setTimeout(() => {
+        window.location.reload();
+      }, 1000);
     }
-    const mySidebarContentElement = document.getElementById("sidebarContent");
+  }, []);
 
-    if (mySidebarContentElement) {
-      mySidebarContentElement.innerHTML = `
-
-    <h1 style="color: white; font-weight: 900;padding-left: 20px;">${"Area Stats"}</h1>
-    <h1 style="color: white; font-weight: 900;"></h1>
-    <div class="spacer"></div>
-
-    <div class="sidebar-box">
-    <p>Temperature: ${userStats.temperature} \u00B0C</p>
-  </div>
-  <div class="sidebar-box">
-  <p>Visibility: ${Math.round(userStats.visibility / 1000)} \u00B0C</p>
-</div>
-  <div class="sidebar-box">
-    <p>Weather: ${userStats.weatherDescp}</p>
-  </div>
-  <div class="sidebar-box">
-  <p>Humidity: ${userStats.humidity} %</p>
-</div>
-<div class="sidebar-box">
-  <p>Rain: ${userStats.rain} mm</p>
-</div>
-<div class="sidebar-box">
-  <p>Clouds: ${userStats.cloudiness} %</p>
-</div>
-<div class="sidebar-box">
-  <p>Snow ${userStats.snow} mm</p>
-</div>
-
-    `;
-    }
-  }
-
-  function closeNav() {
-    const mySidebarElement = document.getElementById("mySidebar");
-    if (mySidebarElement) {
-      mySidebarElement.style.width = "0";
-    }
-  }
-
-  return (
-    <>
-      <div id="mySidebar" className="sidebar">
-        <a className="closebtn" onClick={closeNav}>
-          ×
-        </a>
-        <div id="sidebarContent" className="sidebar-content"></div>
-      </div>
-
-      <button className="openbtn" onClick={openNav}>
-        ☰
-      </button>
-      <div ref={mapNode} style={{ width: "100%", height: "80%" }} />
-    </>
-  );
+  return <div ref={mapNode} style={{ width: "100%", height: "100%" }} />;
 }
 
 export default MapboxMap;
